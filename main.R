1003c3452bf547236a48247c190db7bbff68a720
Jessica Leiria Schattschneider <jessicals@spc.int>
2026-03-11 06:12:06 +1100

    yrs_long and fix bug create maps

--- main.R
+++ main.R
@@ -11,7 +11,7 @@ source("utils.R")
 get_all_country_codes()
 
 # Step 2: Define params ####
-country_codes <- get_all_country_codes()
+country_codes <- c("TO")
 r_year <- 2024
 report_author <- "Jessica LS"
 rewrite_files <- FALSE
@@ -22,11 +22,13 @@ aceman = FALSE # if TRUE, source only ACEs data from member country, if false, s
 
 # Use the params above to download data from T2 and Ikasavea or read data if available in your computer
 for (country_code in country_codes) {
-  create_maps(country_code = country_code, 
-              r_year = r_year)
   process_country_data(country_code = country_code,
                        r_year = r_year,
                        rewrite_files = rewrite_files)
+  yrs_long <- (r_year - 4):r_year
+  create_maps(country_code = country_code, 
+              r_year = r_year)
+  
 }
 
 # Step 4: Generate reports ####
